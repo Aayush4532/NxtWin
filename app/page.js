f3b4c88176f7a917819d43c053eb7bc8ec62137c
Divyansh Singh <142298579+divyanshxcode@users.noreply.github.com>
2025-08-14 00:42:08 +0530

    Cover

--- app/page.js
+++ app/page.js
@@ -1,23 +1,7 @@
 "use client";
 
-import Home from "./components/Home";
-import DashBoard from "./pages/Dashboard";
-import { useUser, UserButton } from "@clerk/nextjs";
-
-export default function Page() {
-  const { isSignedIn, isLoaded } = useUser();
-  return !isSignedIn ? (
-    <>
-      <Home />
-      {!isLoaded && (
-        <div className="absolute inset-0 bg-black/50 flex items-center justify-center z-50">
-          <div className="animate-spin rounded-full h-12 w-12 border-t-4 border-green-400"></div>
-        </div>
-      )}
-    </>
-  ) : (
-    <>
-      <DashBoard />
-    </>
-  );
+function page() {
+  return <div>Removed</div>;
 }
+
+export default page;
